3fe2451142df6584129cf9bc2aa08f752bae6218
Peter Jenkin <peternjenkin@gmail.com>
2019-04-08 14:12:13 +0100

    12-97 New ViewController added, set as entry View, with tableview and nav add button, and segue to MapViewController

---
+++ firstViewController.swift
@@ -0,0 +1,60 @@
+//
+//  firstViewController.swift
+//  Travel Map
+//
+//  Created by Peter Jenkin on 08/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class firstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+ 
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5        // 5 rows, say
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "checking text"
+        return cell
+    }
+    
+    @IBAction func addBtnClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toMapVC", sender: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
