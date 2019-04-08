f21a7deb003eea595124ebcae14b50c80dea33fb
Peter Jenkin <peternjenkin@gmail.com>
2019-04-08 14:37:15 +0100

    12-98 Retrieve CoreData of location info for display in rows/cells in tableView

--- firstViewController.swift
+++ firstViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import CoreData
 
 class firstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     
     @IBOutlet weak var tableView: UITableView!
     
-    
+    var titleArray = [String]()
+    var subtitleArray = [String]()
+    var latitudeArray = [Double]()
+    var longitudeArray = [Double]()
     
     override func viewDidLoad() {
  
@@ -24,20 +28,80 @@ class firstViewController: UIViewController, UITableViewDelegate, UITableViewDat
         tableView.delegate = self
         tableView.dataSource = self
         
+        fetchData()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
+    // bespoke function
+    func fetchData()
+    {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        // NB autocomplete trying to write AppDelegate not appDelegate
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Locations")
+        // cf Entity entry in Travel_Map.xcdatamodeld
+        // NB add <NSFetchRequestResult> template specifier manually - ensure autocomplete doesn't pick wrong one!
+        request.returnsObjectsAsFaults = false
+        do
+        {
+            let results = try context.fetch(request)
+            if results.count > 0
+            {
+                // clear all arrays
+                self.titleArray.removeAll(keepingCapacity: false)
+                self.subtitleArray.removeAll(keepingCapacity: false)
+                self.latitudeArray.removeAll(keepingCapacity: false)
+                self.longitudeArray.removeAll(keepingCapacity: false)
+                
+                
+                for result in results as! [NSManagedObject]
+                {
+                    if let title = result.value(forKey: "title") as? String
+                    {
+                            self.titleArray.append(title)
+                    }
+                    if let subtitle = result.value(forKey: "subtitle") as? String
+                    {
+                        self.subtitleArray.append(subtitle)
+                    }
+                    if let latitude = result.value(forKey: "latitude") as? Double
+                    {
+                        self.latitudeArray.append(latitude)
+                    }
+                    if let longitude = result.value(forKey: "longitude") as? Double
+                    {
+                        self.longitudeArray.append(longitude)
+                    }
+                    print("got here")
+                        self.tableView.reloadData() // refresh table view with retrieved results
+
+                }
+            }
+        }
+        catch
+        {
+            
+        }
+    }
+    
+    
+// delegates added for tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5        // 5 rows, say
+        // return 5        // 5 rows, say
+        return titleArray.count     // however many as are in the titleArray
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "checking text"
+        //cell.textLabel?.text = "checking text"
+        cell.textLabel?.text = titleArray[indexPath.row]
         return cell
     }
     
